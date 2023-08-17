4bce943b13b26dd48196ee1c569144f4002b0fd1
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-17 11:47:51 +0200

    Add extension for automatic DI registration of filters and sorters.

---
+++ PantryOrganizer.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PantryOrganizer.Application.Query;
+using System.Reflection;
+
+namespace PantryOrganizer.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddFiltersFromAssemblies(
+        this IServiceCollection services,
+        IEnumerable<Assembly> assemblies,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+    {
+        foreach (var assembly in assemblies)
+            services.AddFiltersFromAssembly(assembly, lifetime, includeInternalTypes);
+
+        return services;
+    }
+
+    public static IServiceCollection AddFiltersFromAssemblyContaining(
+        this IServiceCollection services,
+        Type type,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddFiltersFromAssembly(type.Assembly, lifetime, includeInternalTypes);
+
+    public static IServiceCollection AddFiltersFromAssemblyContaining<T>(
+        this IServiceCollection services,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddFiltersFromAssembly(typeof(T).Assembly, lifetime, includeInternalTypes);
+
+    public static IServiceCollection AddFiltersFromAssembly(
+        this IServiceCollection services,
+        Assembly assembly,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddTypesFromAssembly(assembly, typeof(IFilter<,>), lifetime, includeInternalTypes);
+
+    public static IServiceCollection AddSortersFromAssemblies(
+        this IServiceCollection services,
+        IEnumerable<Assembly> assemblies,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+    {
+        foreach (var assembly in assemblies)
+            services.AddSortersFromAssembly(assembly, lifetime, includeInternalTypes);
+
+        return services;
+    }
+
+    public static IServiceCollection AddSortersFromAssemblyContaining(
+        this IServiceCollection services,
+        Type type,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddSortersFromAssembly(type.Assembly, lifetime, includeInternalTypes);
+
+    public static IServiceCollection AddSortersFromAssemblyContaining<T>(
+        this IServiceCollection services,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddSortersFromAssembly(typeof(T).Assembly, lifetime, includeInternalTypes);
+
+    public static IServiceCollection AddSortersFromAssembly(
+        this IServiceCollection services,
+        Assembly assembly,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+        => services.AddTypesFromAssembly(
+            assembly,
+            typeof(ISorter<,>),
+            lifetime,
+            includeInternalTypes);
+
+    private static IServiceCollection AddTypesFromAssembly(
+        this IServiceCollection services,
+        Assembly assembly,
+        Type typeToScan,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped,
+        bool includeInternalTypes = false)
+    {
+        AssemblyScanner
+            .FindTypesInAssembly(assembly, typeToScan, includeInternalTypes)
+            .ForEach(scanResult => services.AddScanResult(scanResult, lifetime));
+
+        return services;
+    }
+
+    private static IServiceCollection AddScanResult(
+        this IServiceCollection services,
+        AssemblyScanner.AssemblyScanResult scanResult,
+        ServiceLifetime lifetime)
+    {
+        services.Add(
+            new ServiceDescriptor(
+                serviceType: scanResult.ScannedType,
+                implementationType: scanResult.ResultType,
+                lifetime: lifetime));
+        services.Add(
+            new ServiceDescriptor(
+                serviceType: scanResult.ResultType,
+                implementationType: scanResult.ResultType,
+                lifetime: lifetime));
+
+        return services;
+    }
+}
---
+++ PantryOrganizer.Application/Query/AssemblyScanner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace PantryOrganizer.Application.Query;
+
+public class AssemblyScanner : IEnumerable<AssemblyScanner.AssemblyScanResult>
+{
+    private readonly IEnumerable<Type> types;
+    private readonly Type typeToScan;
+
+    public AssemblyScanner(IEnumerable<Type> types, Type typeToScan)
+    {
+        this.types = types;
+        this.typeToScan = typeToScan;
+    }
+
+    public static AssemblyScanner FindTypesInAssembly(
+        Assembly assembly,
+        Type typeToScan,
+        bool includeInternalTypes = false)
+        => new(includeInternalTypes ? assembly.GetTypes() : assembly.GetExportedTypes(),
+            typeToScan);
+
+    public static AssemblyScanner FindValidatorsInAssemblies(
+        IEnumerable<Assembly> assemblies,
+        Type typeToScan,
+        bool includeInternalTypes = false)
+    {
+        var types = assemblies
+            .SelectMany(x => includeInternalTypes ? x.GetTypes() : x.GetExportedTypes())
+            .Distinct();
+        return new AssemblyScanner(types, typeToScan);
+    }
+
+    private IEnumerable<AssemblyScanResult> Execute()
+    {
+        foreach (var type in types)
+        {
+            if (type.IsAbstract || type.IsGenericTypeDefinition)
+                continue;
+
+            var matchingInterface = type.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == (Type?)typeToScan)
+                .FirstOrDefault();
+
+            if (matchingInterface != null)
+                yield return new AssemblyScanResult(matchingInterface, type);
+        }
+    }
+
+    public void ForEach(Action<AssemblyScanResult> action)
+    {
+        foreach (var result in this)
+        {
+            action(result);
+        }
+    }
+
+    public IEnumerator<AssemblyScanResult> GetEnumerator()
+        => Execute().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+
+    public class AssemblyScanResult
+    {
+        public AssemblyScanResult(Type scannedType, Type resultType)
+        {
+            ScannedType = scannedType;
+            ResultType = resultType;
+        }
+
+        public Type ScannedType { get; private set; }
+
+        public Type ResultType { get; private set; }
+    }
+}
