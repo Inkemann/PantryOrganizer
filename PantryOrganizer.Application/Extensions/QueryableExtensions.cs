33591efb88db4251f0d55ae4062a5a587eb12bda
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-17 11:24:34 +0200

    Add extensions methods for filtering, sorting and paginating IQueryable.

---
+++ PantryOrganizer.Application/Extensions/QueryableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using PantryOrganizer.Application.Query;
+
+namespace PantryOrganizer.Application.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<TData> Filter<TFilter, TData>(
+        this IQueryable<TData> query,
+        IFilter<TFilter, TData> filter,
+        TFilter? filterData)
+        => filter.Apply(query, filterData);
+
+    public static IOrderedQueryable<TData> Sort<TSorting, TData>(
+        this IQueryable<TData> query,
+        ISorter<TSorting, TData> sorter,
+        TSorting? sortingData)
+        => sorter.Apply(query, sortingData);
+
+    public static IQueryable<TData> Paginate<TData>(
+        this IQueryable<TData> query,
+        IPagination? pagination)
+        => pagination != null ?
+            query.Skip((pagination.Page - 1) * pagination.ItemsPerPage)
+                .Take(pagination.ItemsPerPage) :
+            query;
+}
