a3a1a6158cc040a6506c804c6c7bd9aefa0fa810
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-17 13:17:16 +0200

    Add service configuration.

---
+++ PantryOrganizer.Application/ServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PantryOrganizer.Data;
+
+namespace PantryOrganizer.Application;
+
+public static class ServiceConfiguration
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        => services
+            .AddDbContext<PantryOrganizerContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+}
