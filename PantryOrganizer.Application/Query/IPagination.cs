bb78f0b1924c81ea896028ba067e9065ce3364ab
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-17 11:24:02 +0200

    Add pagination.

---
+++ PantryOrganizer.Application/Query/IPagination.cs
@@ -0,0 +1,7 @@
+﻿namespace PantryOrganizer.Application.Query;
+
+public interface IPagination
+{
+    public int Page { get; set; }
+    public int ItemsPerPage { get; set; }
+}
---
+++ PantryOrganizer.Application/Query/Pagination.cs
@@ -0,0 +1,7 @@
+﻿namespace PantryOrganizer.Application.Query;
+
+public record Pagination : IPagination
+{
+    public int Page { get; set; }
+    public int ItemsPerPage { get; set; }
+}
