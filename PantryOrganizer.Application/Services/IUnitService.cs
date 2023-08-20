f181401006791d87a0db5f8ac13c7c1e23490b11
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-20 22:17:52 +0200

    Add unit conversion.

--- PantryOrganizer.Application/Services/IUnitService.cs
+++ PantryOrganizer.Application/Services/IUnitService.cs
@@ -4,4 +4,13 @@ namespace PantryOrganizer.Application.Services;
 
 public interface IUnitService :
     IDataService<UnitDto, Guid, UnitSortingDto, UnitFilterDto>
-{ }
+{
+    public ConversionResult GetConversionRate(Guid fromId, Guid toId);
+
+    public ConversionResult GetBaseConversion(Guid unitId);
+
+    public record ConversionResult(UnitDto? Base, double? ConversionRate)
+    {
+        public ConversionResult() : this(null, null) { }
+    }
+}
--- PantryOrganizer.Application/Services/UnitService.cs
+++ PantryOrganizer.Application/Services/UnitService.cs
@@ -19,4 +19,55 @@ public class UnitService :
         IFilter<UnitFilterDto, Unit> filter)
         : base(context, mapper, validator, sorting, filter)
     { }
+
+    public IUnitService.ConversionResult GetConversionRate(Guid fromId, Guid toId)
+    {
+        var from = context.Set<Unit>().SingleOrDefault(item => item.Id == fromId);
+        var to = context.Set<Unit>().SingleOrDefault(item => item.Id == toId);
+
+        if (from == null || to == null
+            || !from.DimensionId.HasValue || from.DimensionId != to.DimensionId)
+        {
+            return new IUnitService.ConversionResult();
+        }
+
+        if (from.IsBase)
+        {
+            return new IUnitService.ConversionResult(
+                mapper.Map<UnitDto>(from),
+                1d / to.BaseConversionFactor);
+        }
+        if (to.IsBase)
+        {
+            return new IUnitService.ConversionResult(
+                mapper.Map<UnitDto>(to),
+                from.BaseConversionFactor);
+        }
+
+        var baseUnit = context.Set<Unit>()
+            .SingleOrDefault(item => item.IsBase && item.DimensionId == from.DimensionId);
+        double? fromConversionRate = from.BaseConversionFactor;
+        double? toConversionRate = 1d / to.BaseConversionFactor;
+
+        return new IUnitService.ConversionResult(
+            mapper.Map<UnitDto>(baseUnit),
+            fromConversionRate * toConversionRate);
+    }
+
+    public IUnitService.ConversionResult GetBaseConversion(Guid unitId)
+    {
+        var unit = context.Set<Unit>().SingleOrDefault(item => item.Id == unitId);
+
+        if (unit == null || !unit.DimensionId.HasValue)
+            return new IUnitService.ConversionResult();
+
+        var baseUnit = unit.IsBase
+            ? unit
+            : context.Set<Unit>()
+                .SingleOrDefault(item => item.IsBase && item.DimensionId == unit.DimensionId);
+
+        return new IUnitService.ConversionResult(
+            mapper.Map<UnitDto>(baseUnit),
+            unit.BaseConversionFactor);
+    }
 }
