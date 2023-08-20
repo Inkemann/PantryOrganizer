2956d19311f096f6a30962605807d8d281569368
Jonathan Inkemann <jonathan.inkemann@rwth-aachen.de>
2023-08-20 11:04:14 +0200

    Add blazor server project.

---
+++ PantryOrganizer.BlazorServer/Program.cs
@@ -0,0 +1,28 @@
+using PantryOrganizer.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
